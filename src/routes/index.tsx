0228b422c71beee6c82c9e0a06e94dbaeb024659
Gabriel Fernandes <gfsouza@poli.ufrj.br>
2020-06-29 21:50:23 -0300

    listagem de repositórios

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,7 @@ const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
     <Route path="/repository" component={Repository} />
+    {/* <Route path="/repository" component={Repository} /> */}
   </Switch>
 );
 
